db788e7f767c1d8bbe6135242890b1e1d287c53b
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2017-09-25 16:07:12 -0500

    Added plugin readme

--- apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class ExampleAppTwoConfig(AppConfig):
-    name = 'example_app_two'
--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.ExampleAppTwoView.as_view(), name='example_app_two'),
-]
