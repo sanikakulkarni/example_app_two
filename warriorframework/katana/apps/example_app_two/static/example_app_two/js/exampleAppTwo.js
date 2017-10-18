4527b2be4cae9188e36ba14959797158590b912b
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2017-10-18 11:46:01 -0500

    Added css files

--- warriorframework/katana/apps/example_app_two/static/example_app_two/js/exampleAppTwo.js
+++ warriorframework/katana/apps/example_app_two/static/example_app_two/js/exampleAppTwo.js
@@ -1,1 +1,10 @@
-var exampleAppTwo = {}
+var exampleAppTwo = {
+    toggleColor: function(){
+        var $elem = $(this);
+        if($elem.hasClass('example-app-two-btn-color')){
+            $elem.removeClass('example-app-two-btn-color');
+        } else {
+            $elem.addClass('example-app-two-btn-color');
+        }
+    }
+}
